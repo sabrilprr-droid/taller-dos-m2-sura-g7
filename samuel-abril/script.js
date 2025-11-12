ab9da69fa49845aa0dd862e974ab11d2433ec051
Samuel abril <sabrilprr@gmail.com>
2025-11-11 22:39:34 -0500

    solucion ejercicio 1

---
+++ samuel-abril/script.js
@@ -0,0 +1,17 @@
+//  ## Ejercicio 1: Duplicar Números (map)
+//  Inicias con una lista de puntos:
+//  `let puntos = [10, 20, 30, 40];`
+//  
+//  Tareas:
+//  1.  Crea un nuevo arreglo `puntosDoble` donde cada valor sea el doble del arreglo original.
+//  2.  Muestra `puntos` y `puntosDoble` para verificar que el original no cambió.
+
+let puntos = [10, 20, 30, 40];
+let puntosDoble = puntos.map(i => i *2); //recuarda que n puede ser cualquier cosa como, m, i, j, o nombre solo va a representar cada elemento del arreglo 
+console.log({puntos,puntosDoble}); //se muestra como objeto de esta forma para depurar y poder comparar el arreglo orginal con el nuevo con el metodo
+
+let puntosDobleDos = puntos.map(function(i) {
+  return i * 2;
+});
+console.log({puntos,puntosDobleDos});
+
